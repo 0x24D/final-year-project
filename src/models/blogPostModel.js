ae72c1872ec3e789df9849b8694c8f180c371c63
0x24d <daniel.longfield@tuta.io>
2019-01-24 20:12:38 +0000

    Add blog post schema

---
+++ src/models/blogPostModel.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+export const BlogPostSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: 'Enter a title'
+  },
+  body: {
+    type: String
+  },
+  tags: {
+    type: Array
+  }
+});
