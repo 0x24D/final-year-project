650ae9f1ebdb18e0e41403587f6c954123761b64
0x24d <daniel.longfield@tuta.io>
2019-04-08 15:56:06 +0100

    Remove vue logo image

--- client/src/pages/index/App.vue
+++ client/src/pages/index/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <NavigationBar/>
-    <img alt="Vue logo" src="../../assets/logo.png">
     <h1> Welcome to the myBlog website </h1>
     <h3> Use the navigation bar above to add a new post or view all current posts </h3>
   </div>
